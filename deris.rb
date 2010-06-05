e724aa24183618de74c392f53d3805ceafb5137b
Garry Shutler <garry@robustsoftware.co.uk>
2010-06-05 22:21:07 +0100

    working example of using haml to render nested templates

---
+++ deris.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'fileutils'
+require 'haml'
+
+module Deris
+
+  def self.render(template)
+    if template.is_a? Symbol
+      template = template.to_s
+    else
+      template = read_file(template)
+    end
+    haml_engine = Haml::Engine.new(template)
+    haml_engine.render(self)
+  end
+  
+  private
+  
+  def self.read_file(filename)
+    f = File.open(filename, "r") 
+    data = ''
+    f.each_line do |line|
+      data += line
+    end
+    data   
+  end
+
+end
+
+if __FILE__ == $0
+  puts Deris::render(ARGV[0])
+end
+
