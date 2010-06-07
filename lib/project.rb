fd464da7dfc9f843112e5bad52c9c5e807d5720e
gshutler <garry@robustsoftware.co.uk>
2010-06-07 15:15:25 +0100

    added copying of static content from the source directory to the output directory

--- lib/project.rb
+++ lib/project.rb
@@ -8,20 +8,27 @@ module Deris
   
     def initialize(source)
       @source = source
-      @content_directory = ::File.join(@source, '_src')
-      @defaults = partials_from @content_directory
+      @source_directory = ::File.join(@source, '_src')
+      @defaults = partials_from @source_directory
     end
     
     def write(output)
+      # recreate the output directory
       FileUtils.rm_rf(output) if ::File.exists?(output)
       FileUtils.mkdir(output)
       
+      # find and copy all static content
+      static_content_mask = ::File.join(@source, "**")
+      static_content = Dir[static_content_mask].reject{|path| path =~ /_src$/}      
+      static_content.each{|path| FileUtils.cp_r(path, output)}
+      
+      # generate the index page from defaults
       File.new('index', @defaults).write output
-            
-      content_directories.each do |dir|
+
+      # generate the other pages of the site from sub-directories
+      source_directories.each do |dir|
         partials = partials_from dir
-        partials = @defaults.merge(partials)
-        
+        partials = @defaults.merge(partials)        
         file_name = ::File.basename(dir)
         File.new(file_name, partials).write output
       end
@@ -29,9 +36,9 @@ module Deris
     
     private
     
-    def content_directories
-      content_directory_mask = ::File.join(@content_directory, '*')
-      ::Dir[content_directory_mask].find_all do |dir|
+    def source_directories
+      source_directory_mask = ::File.join(@source_directory, '*')
+      ::Dir[source_directory_mask].find_all do |dir|
         ::File.directory?(dir)
       end      
     end
